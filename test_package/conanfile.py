c33351235e5abc861fd3a281783e640f999bd248
Maximilian Mühlbauer <maximilian.muehlbauer@dlr.de>
2024-03-01 11:37:02 +0100

    test: conan2 compat

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -6,13 +6,18 @@ from conan.tools.build import can_run
 
 
 class TestTestConan(ConanFile):
+    test_type = "explicit"
     settings = "os", "compiler", "build_type", "arch"
     exports_sources = "mod_test.rkc"
     generators = "VirtualRunEnv"
 
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
     def test(self):
-        rkc_path = os.path.join(self.source_folder, "mod_test.rkc")
-        shutil.copy(rkc_path, "mod_test.rkc")
+        if self.source_folder != self.build_folder:
+            rkc_path = os.path.join(self.source_folder, "mod_test.rkc")
+            shutil.copy(rkc_path, "mod_test.rkc")
 
         if can_run(self):
             self.run("robotkernel --test-run --config .%smod_test.rkc" % os.sep, env="conanrun")
