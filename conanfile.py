4060093074e87190ef4943661582dc8c5e4d3a42
Robert Burger <robert.burger@dlr.de>
2025-07-17 12:07:34 +0200

    fix: generate of AC_INIT

--- conanfile.py
+++ conanfile.py
@@ -10,3 +10,7 @@ class MainProject(ConanFile):
     exports_sources = ["*", "!.gitignore"]
     tool_requires = ["robotkernel_service_helper/[~6]@robotkernel/unstable"]
     requires = ["robotkernel/[~6]@robotkernel/unstable", "service_provider_process_data_inspection/[~6]@robotkernel/unstable"]
+    
+    def source(self):
+        self.run(f"sed 's/AC_INIT(.*/AC_INIT([robotkernel], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
+
