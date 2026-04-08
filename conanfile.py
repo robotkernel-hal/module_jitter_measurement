f2adf9faf0b451eaaea4fe1b28a1d849bf760d9b
Robert Burger <robert.burger@dlr.de>
2026-04-08 16:02:16 +0200

    fix: build system with conan_template 6

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,13 @@
 from conan import ConanFile
 
-
 class MainProject(ConanFile):
-    python_requires = "conan_template/[^5.0.6]@robotkernel/stable"
+    python_requires = "conan_template/[^6]@robotkernel/unstable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_jitter_measurement"
     description = "robotkernel jitter measurement module."
     exports_sources = ["*", "!.gitignore"]
     tool_requires = ["robotkernel_generator/[~6]@robotkernel/unstable"]
-
-    def source(self):
-        self.run(f"sed 's/AC_INIT(.*/AC_INIT([robotkernel], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
 
     def requirements(self):
         self.requires(f"{self.name}_ln_msgdef/{self.version}@{self.user}/{self.channel}")
