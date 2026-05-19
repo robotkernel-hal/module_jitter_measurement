e7850208410cc8c62e3a872ecdbc83282dd84926
Robert Burger <robert.burger@dlr.de>
2026-05-19 08:41:17 +0200

    fix: restructured build

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,16 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[^6]@robotkernel/unstable"
+    python_requires = "conan_template/[^6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_jitter_measurement"
     description = "robotkernel jitter measurement module."
     exports_sources = ["*", "!.gitignore"]
-    tool_requires = ["robotkernel_generator/[~6]@robotkernel/unstable"]
+    tool_requires = ["robotkernel_generator/[~6]@robotkernel/stable"]
 
     def requirements(self):
         self.requires(f"{self.name}_ln_msgdef/{self.version}@{self.user}/{self.channel}")
-        self.requires("service_provider_process_data_inspection/[~6]@robotkernel/unstable")
-        self.requires("robotkernel/[~6]@robotkernel/unstable")
+        self.requires("service_provider_process_data_inspection/[~6]@robotkernel/stable")
+        self.requires("robotkernel/[~6]@robotkernel/stable")
 
